7fcc822d073d1a34be8062848d7d8edd8569775a
Alexander Dubovik <Alexander.Dubovik@darkmatter.ae>
2019-09-28 18:43:46 +0300

    Minor fix

--- SimpleZomby/SimpleZomby.cpp
+++ SimpleZomby/SimpleZomby.cpp
@@ -5,7 +5,7 @@
 void SimpleZomby::runSomethingAsync()
 {
     _future = std::async(std::launch::async, [shis = shared_from_this()](){
-        while(true) {
+        while(shis) {
             std::cout << "Zomby is alive!" << std::endl;
             std::this_thread::sleep_for(std::chrono::milliseconds(1000));
         }
