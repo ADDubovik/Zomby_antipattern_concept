64e486a46e969d54c7387f3e50b0a50a6a7c41a0
Alexander Dubovik <Alexander.Dubovik@darkmatter.ae>
2019-10-20 11:06:27 +0300

    #includes change

--- PimplCyclic/PimplCyclic.cpp
+++ PimplCyclic/PimplCyclic.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <functional>
 
 #include "PimplCyclic.h"
 
--- PimplCyclic/PimplCyclic.h
+++ PimplCyclic/PimplCyclic.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <memory>
-#include <functional>
 
 namespace PimplCyclic {
 class Cyclic
