b8ace90f312796d297119e9c63432355d9b98a08
Alexander Dubovik <Alexander.Dubovik@darkmatter.ae>
2019-10-28 10:59:33 +0300

    Format fix

--- boozd/azzio/io_context.cpp
+++ boozd/azzio/io_context.cpp
@@ -21,15 +21,15 @@ void io_context::run()
 {
     if (_pack_optional && _handler_optional) {
         auto& [s, b] = *_pack_optional;
-		using namespace std::chrono;
+        using namespace std::chrono;
         auto start = steady_clock::now();
-		while (duration_cast<milliseconds>(steady_clock::now() - start).count() < 1000) {
+        while (duration_cast<milliseconds>(steady_clock::now() - start).count() < 1000) {
             if (auto read = s.read())
                 b.emplace_back(*read);
             std::this_thread::sleep_for(milliseconds(100));
         }
 
         (*_handler_optional)(error_code::no_error);
-	}
+    }
 }
 } // namespace boozd::azzio
