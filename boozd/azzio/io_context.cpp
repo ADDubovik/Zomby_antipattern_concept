5366df999d61efaf52e51c33b1e65af7be6bfa80
Alexander Dubovik <Alexander.Dubovik@darkmatter.ae>
2019-10-28 10:53:12 +0300

    Naming fix

--- boozd/azzio/io_context.cpp
+++ boozd/azzio/io_context.cpp
@@ -11,21 +11,21 @@ io_context::~io_context()
     std::cout << typeid(*this).name() << "::" << __func__ << std::endl;
 }
 
-void io_context::async_read(stream& s, buffer& buf, io_context::handler&& h)
+void io_context::async_read(stream& s, buffer& b, io_context::handler&& handler)
 {
-    _pack_optional.emplace(buf, s);
-    _handler_optional.emplace(std::move(h));
+    _pack_optional.emplace(s, b);
+    _handler_optional.emplace(std::move(handler));
 }
 
 void io_context::run()
 {
     if (_pack_optional && _handler_optional) {
-        auto& [buf, s] = *_pack_optional;
+        auto& [s, b] = *_pack_optional;
 		using namespace std::chrono;
         auto start = steady_clock::now();
 		while (duration_cast<milliseconds>(steady_clock::now() - start).count() < 1000) {
             if (auto read = s.read())
-                buf.emplace_back(*read);
+                b.emplace_back(*read);
             std::this_thread::sleep_for(milliseconds(100));
         }
 
--- boozd/azzio/io_context.h
+++ boozd/azzio/io_context.h
@@ -19,13 +19,13 @@ public:
     // Start an asynchronous operation to read a certain amount of data from a stream.
     // This function is used to asynchronously read a certain number of bytes of data from a stream.
     // The function call always returns immediately.
-    void async_read(stream& s, buffer& buf, handler&& h);
+    void async_read(stream& s, buffer& b, handler&& handler);
 
     // Run the io_context object's event processing loop.
     void run();
 
 private:
-    using pack = std::tuple<buffer&, stream&>;
+    using pack = std::tuple<stream&, buffer&>;
     using pack_optional = std::optional<pack>;
     using handler_optional = std::optional<handler>;
 
