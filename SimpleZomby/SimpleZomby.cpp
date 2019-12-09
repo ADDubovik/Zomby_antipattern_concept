e1dc39c5791fb4148ff95ccd2f4ed8a1924047f2
Alexander Dubovik <Alexander.Dubovik@darkmatter.ae>
2019-12-09 18:33:45 +0300

    SimpleZomby - semaphore check removal

--- SimpleZomby/SimpleZomby.cpp
+++ SimpleZomby/SimpleZomby.cpp
@@ -37,7 +37,7 @@ void Zomby::runOnce(std::shared_ptr<Common::Listener> listener)
 
     _thread = std::thread([whis = weak_from_this()](){
         while (auto shis = whis.lock()) {
-            if (shis->_listener && shis->_semaphore) {
+            if (shis->_listener) {
                 shis->_listener->processData(std::make_shared<Common::Listener::Data>("SimpleZomby is alive!\n"));
                 std::this_thread::sleep_for(std::chrono::seconds(1));
             }
